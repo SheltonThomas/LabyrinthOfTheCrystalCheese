ed7a539cd87c1c65141b857020892b363f990f80
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-05-18 18:23:42 -0500

    Made a scene and PlayPauseBehavior
    Made a scene for me to work in and added a PlayPauseBehavior

---
+++ Assets/Scripts/PlayPauseBehavior.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPauseBehavior : MonoBehaviour
+{
+    public static bool Paused { get; set; }
+}
