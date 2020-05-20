f890d74cac6d301b7b83ee4b9e517f9a86db80a7
Joseph Kim <54289189+kimjo562@users.noreply.github.com>
2020-05-20 15:45:57 -0500

    Cat, Mouse, Traps
    (Long Commit due to Github Technical difficulties)
    
    Created a Movement Script for both the Cat and Mouse, The Cat now has a working AI and uses NavMesh and NavMesh Agent to Navigate and Follow the Player, The Player can now place traps (WIP), Player Uses a Keyboard Movement Behavior to also move around.

---
+++ Assets/Scripts/KeyboardMovementBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovementBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Find the Direction
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+            moveDirection += new Vector3(0, 0, 1);   // Up
+        if (Input.GetKey(KeyCode.A))
+            moveDirection += new Vector3(-1, 0, 0);  // Left
+        if (Input.GetKey(KeyCode.S))
+            moveDirection += new Vector3(0, 0, -1);   // Down
+        if (Input.GetKey(KeyCode.D))
+            moveDirection += new Vector3(1, 0, 0);   // Right
+        moveDirection.Normalize();
+
+        // Set Magnitude
+        moveDirection *= speed;
+
+        // Move
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpawnTrapBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrapBehavior : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public Transform behaviourTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            spawnTrap();
+        }
+    }
+
+    void spawnTrap()
+    {
+        // Spawn an instance of the objectToSpawn
+        GameObject spawnedInstance = Instantiate(objectToSpawn, transform.position, transform.rotation);
+        // Set the TrapBehaviour's target to the behaviourTarget
+        // TrapBehavior trapBehavior = spawnedInstance.GetComponent<TrapBehavior>();
+        //if (trapBehavior != null)
+        //{
+        //    trapBehavior.target = behaviourTarget;
+        //}
+    }
+}
