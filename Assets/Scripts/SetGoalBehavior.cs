a15cd2bc4f51b3f9655d966d9c2c1f1a71441cd0
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-05-20 17:36:55 -0500

    Added Set Goal
    Added a scrpt that allows players to set a goal

---
+++ Assets/Scripts/SetGoalBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetGoalBehavior : MonoBehaviour
+{
+    private string gameMode;
+    [SerializeField]
+    private Text goalText;
+
+    private float? previousNumber;
+    [SerializeField]
+    private InputField inputField;
+
+    void Start()
+    {
+        MainMenuBehavior.GameMode = GameMode.Points;
+
+        if (MainMenuBehavior.GameMode == GameMode.Points)
+        {
+            gameMode = "point";
+        }
+        if (MainMenuBehavior.GameMode == GameMode.Timed)
+        {
+            gameMode = "time";
+        }
+        SetGoalText();
+    }
+
+    public void SetGoalText()
+    {
+        goalText.text = "Type the desired " + gameMode + " limit.";
+    }
+
+    public void CheckForCharacter()
+    {
+        if(inputField.text.Length == 0)
+        {
+            previousNumber = null;
+            return;
+        }
+
+        char characterToCheck = inputField.text[inputField.text.Length - 1];
+        int intCheck;
+        if (int.TryParse(characterToCheck.ToString(), out intCheck))
+        {
+            previousNumber = int.Parse(inputField.text);
+        }
+        else
+        {
+            inputField.text = previousNumber.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/TimerBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+    [HideInInspector]
+    public float startTime = 0;
+    private float timeRemaining;
+    public float TimeRemaining { get; }
+
+    void Start()
+    {
+        timeRemaining = startTime;
+    }
+
+    void Update()
+    {
+        DrawTimer();
+        UpdateTimer();
+    }
+
+    public void UpdateTimer()
+    {
+        if (!PauseMenuBehavior.Paused)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+    }
+
+    public void DrawTimer()
+    {
+        timerText.text = (Math.Round(timeRemaining, 1)).ToString();
+    }
+}
