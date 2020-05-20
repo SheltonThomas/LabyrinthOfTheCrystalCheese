b6f9fe583cef63ab9a0491c2605c60f5d8c0558c
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-05-20 12:45:44 -0500

    Made PauseMenuBehavior and puase menu
    Made the behavior script for the pause menuand made the menu appear and disappear as needed

---
+++ Assets/Scripts/PauseMenuBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehavior : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public static bool Paused { get; set; }
+
+    void Start()
+    {
+        pauseMenu.SetActive(Paused);
+    }
+
+    void Update()
+    {
+        if(Paused)
+        {
+            pauseMenu.SetActive(Paused);
+        }
+    }
+    
+    public void ResumeGame()
+    {
+        Paused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void QuitGame(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
