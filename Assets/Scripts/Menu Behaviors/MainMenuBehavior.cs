d73a6265f51d22dd923abb8ed7b8584bc736b17e
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-05-20 20:42:02 -0500

    Updated MainMenuBehavior
    Made it so clicking a game mode brings you to the screen to set a limit

--- Assets/Scripts/Menu Behaviors/MainMenuBehavior.cs
+++ Assets/Scripts/Menu Behaviors/MainMenuBehavior.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public SetGoalBehavior setGoalBehavior;
+
     public void PlayGame(int gameMode)
     {
         GameVariables.GameMode = (GameMode)gameMode;
+        setGoalBehavior.gameObject.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     public void Quit()
