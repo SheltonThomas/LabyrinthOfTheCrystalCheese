729d5a92239be0058044a35c2022daa23766b9be
Shelton Thomas <sheltonjthomasjr@gmail.com>
2020-05-22 13:00:11 -0500

    Added MapZoomBehavior
    Map zooms and can detect if an object is off the screen

---
+++ Assets/Scripts/Game Behaviors/MapZoomBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapZoomBehavior : MonoBehaviour
+{
+    public Transform camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = camera.position - gameObject.transform.position;
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * Time.deltaTime * 5);
+    }
+}
---
+++ Assets/Scripts/Game Behaviors/OutOfBoundsBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBoundsBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private List<Collider> targets;
+    [SerializeField]
+    private Camera cam;
+
+    private Plane[] planes;
+
+    private void Start()
+    {
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Collider target in targets)
+        {
+            if (!target.gameObject.activeInHierarchy)
+                continue;
+
+            if(!GeometryUtility.TestPlanesAABB(planes, target.bounds))
+            {
+                Debug.Log(target.gameObject.name + " is off the screen.");
+                target.gameObject.SetActive(false);
+            }
+        }
+    }
+}
