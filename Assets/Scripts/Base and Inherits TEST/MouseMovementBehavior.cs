ebd7ded6275bb2db92fe958a9351ed4b9ba0de8f
Joseph Kim <54289189+kimjo562@users.noreply.github.com>
2020-06-29 11:54:30 -0500

    Added 6 more scripts
    Added 2 Base scripts: Movement, and Traps
    
    Added 4 Deviations
    Cat Movement
    Cat TrapPlacement
    Mouse Movement
    MouseTrapPlacement

---
+++ Assets/Scripts/Base and Inherits TEST/MouseMovementBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovementBehavior : MonoBehaviour
+{
+    private MovementBaseBehavior movementBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementBehavior.rb = this.GetComponent<Rigidbody>();
+        movementBehavior.prevSpeed = movementBehavior.speed;
+        movementBehavior.currentSpeed = movementBehavior.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetButton("Horizontal") && !Input.GetButton("Vertical"))
+        {
+            movementBehavior.rb.constraints = RigidbodyConstraints.FreezeRotation |
+                            RigidbodyConstraints.FreezePositionX |
+                            RigidbodyConstraints.FreezePositionY;
+            return;
+        }
+        movementBehavior.rb.constraints = RigidbodyConstraints.None;
+
+        // Gets Facing Direction
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Facing to move with same direction
+        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        newPosition.Normalize();
+        transform.LookAt(newPosition + transform.position);
+        transform.Translate(newPosition * movementBehavior.speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/Base and Inherits TEST/SpawnTrapBaseBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrapBaseBehavior : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private TrapDetectionBehavior trapBehavior;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTrap();
+    }
+
+    void spawnTrap()
+    {
+        // Spawn an instance of the objectToSpawn
+        GameObject spawnedInstance = Instantiate(objectToSpawn, transform.position, transform.rotation);
+        spawnedInstance.GetComponent<TrapDetectionBehavior>().Owner = transform.parent.gameObject.name;
+    }
+}
